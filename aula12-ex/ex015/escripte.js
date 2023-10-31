57699cdeaa7c8698152318ef59497e140df6e1d2
Guillam07 <gui03marques@gmail.com>
2023-10-31 02:41:59 -0300

    workInProgress

---
+++ aula12-ex/ex015/escripte.js
@@ -0,0 +1,26 @@
+function verificar(){
+    var data = new Date()
+
+    var ano = data.getFullYear()
+    var texto = document.getElementById('txtano').value
+    var resp = document.querySelector('div#res')
+
+    if(texto.length == 0 || texto > ano){
+        window.alert('[ERRO] Verifique os dados e tente novamente')
+    }
+    else{
+        var sex = document.getElementsByName('radsex')
+        var idade = ano - Number(texto)
+        
+        var genero = '';
+        if (sex[0].checked){
+            genero = 'Homem'
+        }
+        else if(sex[1].checked){
+            genero = 'Mulher'
+        }
+        resp.style.textAline = 'center';
+        resp.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+
+    }
+}
